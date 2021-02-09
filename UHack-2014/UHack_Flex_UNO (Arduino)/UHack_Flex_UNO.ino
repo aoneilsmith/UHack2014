3968235dd162767331e8637f41d52503fc85bac1
Andrew <aosx182@gmail.com>
2021-02-09 00:54:43 -0500

    Initial Commit

---
+++ UHack-2014/UHack_Flex_UNO (Arduino)/UHack_Flex_UNO.ino
@@ -0,0 +1,124 @@
+/*
+  Andrew O'Neil-Smith
+  UHack 2014
+  Zensah Arduino Flex Hack
+*/
+
+// HARDWARE:
+// Make the following connections between the Arduino and the flex sensor
+// Note that the flex sensor pins are interchangeable
+
+// Sensor pin - GND
+// Sensor pin - Analog In 0, with 10K resistor to +5V
+
+// INSTRUCTIONS:
+// Upload this sketch to your Arduino, then activate the Serial Monitor (control + shift + m)
+// (set the Serial Monitor to 9600 baud)
+
+#include "pitches.h";
+
+void setup()
+{
+  // initialize serial communications
+  Serial.begin(9600);
+  //Serial1.begin(9600); 
+}
+
+void loop()
+{
+  int sensor, degrees; //right sensor
+  int sensorL, degreesL; //left sensor
+  
+  // read the voltage from the voltage divider (sensor plus resistor)
+  sensor = analogRead(0); //right sensor is on A0
+  sensorL = analogRead(1); //left sensor is on A1
+  
+  // convert the voltage reading to inches
+  // the first two numbers are the sensor values for straight (724) and bent (853)
+  // the second two numbers are the degree readings we'll map that to (0 to 90 degrees)
+  degrees = map(sensor, 724, 853, 0, 90); //right sensor
+  degreesL = map(sensorL, 730, 822, 0, 90); //left sensor
+  
+  /* your sensor's values will vary
+     to improve the accuracy, run the program, note your sensor's analog values when it's straight and bent
+     insert those values into the above function.
+   */
+  
+  //the following will be sent to Processing to be read for RGB and Arm Angle
+  int val = map(sensor, 724, 853, 0, 255); // mapping the sensor (10 bit 0-1023) to RGB values (8 bit 0-255)
+  
+   //not allowing Right sensor values outside of 0-255 be sent to Processing
+    if(val<=0)
+      val = 0;
+    else if (val>=255)
+      val = 255;
+      //Serial.println(val);  //debug    
+
+  //not allowing Left sensor values outside of 0-255 be sent to Processing
+  int valL = map(sensorL, 724, 853, 0, 255); //10 to 8 bit
+    if(valL<=0)
+      valL = 0;
+    else if (valL>=255)
+      valL = 255;
+  //Serial.println(valL); //debug
+  
+  //Prints to the serial monitor Left and Right 0-255 values
+  //will be read by Processing for RBG and Arm Angle
+  Serial.print(valL);
+  Serial.print(" ");
+  Serial.println(val);
+  
+  //Right = melody
+  if(degrees > 0 && degrees < 20){
+    tone(8,NOTE_A4,120); //plays an A on pin 8 for 120ms
+      delay(120);                     
+      }
+ else if(degrees >= 20 && degrees < 40){
+    tone(8,NOTE_CS5,120);
+      delay(120);
+    }
+  else if(degrees >= 40 && degrees < 50){
+    tone(8,NOTE_E5,120);
+    delay(120);                     
+    }
+  else if( degrees >= 50){
+    tone(8,NOTE_A5, 120);
+    delay(120);                     
+    }
+     
+  //Left = bass
+ if(degreesL > 0 && degreesL < 20){
+    tone(8,NOTE_A2,200);
+      delay(240);                     
+      }
+ else if(degreesL >= 20 && degreesL < 40){
+    tone(8,NOTE_CS3,200);
+      delay(240);
+    }
+  else if(degreesL >= 40 && degreesL < 50){
+    tone(8,NOTE_E3,200);
+    delay(240);                     
+    }
+  else if( degreesL >= 50){
+    tone(8,NOTE_A3, 200);
+   delay(240); 
+  }
+    
+    
+  /*
+  // debug output if you want to view 
+  Serial.print("analog input: ");
+  Serial.print(sensor,DEC);
+  Serial.print("   degrees: ");
+  Serial.println(degrees,DEC);
+
+  Serial1.print("analog input: ");
+  Serial1.print(sensorL,DEC);
+  Serial1.print("   degrees: ");
+  Serial1.println(degreesL,DEC);  
+  
+  */
+  
+  delay(100);   // pause before taking the next reading to make datat easier to read
+                    
+}
---
+++ UHack-2014/UHack_Flex_Uno (Processing)/UHack_Flex_Uno.pde
@@ -0,0 +1,70 @@
+/*
+  Andrew O'Neil-Smith
+  UHack 2014
+  Zensah Arduino Flex Hack
+*/
+
+import org.firmata.*;
+import cc.arduino.*;
+import processing.serial.*;
+
+Serial myPort;
+float greenness;
+float seglength = 100;
+float x, y;
+float angle1 = 0.0;
+float angle2 = 0.0;
+float segLength = 100;
+
+void setup()
+{
+ size(640,360); //window size
+ 
+ //setting the port to read COM 3. change based on what COM port your arduino comes in on.
+ //COM port is called something different if you are using Mac OS
+ myPort= new Serial(this, "COM3", 9600);
+ 
+ myPort.bufferUntil('\n'); //doesnt read data until it starts streaming from arduino
+ 
+ //arm setup
+ strokeWeight(30);
+ stroke(255, 160);
+ x = width * 0.3;
+ y = height * 0.5;
+}
+
+void draw()
+{
+  
+  background(0,greenness,0); //draws background with RGB colors
+  
+  angle1 = (mouseX/float(width) - 0.5) * -PI;  //angle 1 rotation is mapped to mouse movement on X axis
+  //angle2 = (mouseY/float(height) - 0.5) * PI; // this will get drawn later
+  
+  //println(mouseY); //debug
+  
+  pushMatrix();
+  segment(x, y, angle1); 
+  segment(segLength,0,angle2);
+  popMatrix();
+}
+
+//every time there is a serial event, this gets called
+void serialEvent(Serial myPort)
+{
+  greenness = float(myPort.readStringUntil(' ')); //the Green RGB value is mapped to the first column of data coming from arduino
+  angle2 = float(myPort.readStringUntil('\n'))/255 * -PI; //the arm angle is mapped to the second column of data coming from the arduino
+  //redness = float(myPort2.readStringUntil('\n')); //
+  segment(x,y,angle2); //draws the forearm segment
+  //println(angle2); //debug
+  //println(redness);
+}
+
+//draw segment funciton
+void segment(float x, float y, float a) {
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+}
+
+
